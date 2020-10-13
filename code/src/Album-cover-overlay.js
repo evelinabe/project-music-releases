c94216cf169b4d2665f3a8e09ab444e266301a5a
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-10-13 16:54:20 +0200

    tried to add image in albumcoveroverlay

--- code/src/Album-cover-overlay.js
+++ code/src/Album-cover-overlay.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import play from '../../public/icons/play.svg'
+import Play from '../../public/icons/play.svg'
+
+//const play = require('../../public/icons/play.svg')
 
 const AlbumCoverOverlay = () => {
     return (
         <div className="album-cover-overlay">
-            <img src={play} alt="play-icon"/>
+            <img src={Play} alt="play-icon"/>
         </div>
     )
 }
