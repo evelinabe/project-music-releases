60f5eaf657022917c4ee6f08f70bffdc2aa8d6f1
Evelina Berg <evelina.berg@outlook.com>
2020-10-14 08:49:06 +0200

    Album hover nearly done

--- code/src/Album-cover-overlay.js
+++ code/src/Album-cover-overlay.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Play from '../../public/icons/play.svg'
-
-//const play = require('../../public/icons/play.svg')
+import Play from './icons/play.svg';
+import Heart from './icons/heart.svg';
+import Dots from './icons/dots.svg';
 
 const AlbumCoverOverlay = () => {
     return (
         <div className="album-cover-overlay">
-            <img src={Play} alt="play-icon"/>
+            <img src={Heart} alt="heart-icon" className="heart-icon icon" />
+            <img src={Play} alt="play-icon" className="play-icon icon"/>
+            <img src= {Dots} alt="dots-icon" className="dots-icon icon" />
         </div>
     )
 }
