8659c460585339e1856e13ac9868c0573ebf0e62
Evelina Berg <evelina.berg@outlook.com>
2020-10-16 13:40:46 +0200

    CSS updated

--- code/src/Page-title.js
+++ code/src/Page-title.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 
-const PageTitle = () => <h1 className="page-title">New Albums & Singles</h1>;
+const PageTitle = () => {
+
+    return (
+        <div className="page-title-wrapper">
+            <h1 className="page-title">New Albums & Singles</h1>;
+        </div>
+    )
+}
 
 export default PageTitle;
