7b9cd80b6480e46298230a1396114bac3275d80c
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-10-16 11:36:52 +0200

    added & before last item

--- code/src/Artist-name.js
+++ code/src/Artist-name.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 
 const ArtistName = (props) => {
-        return (
-            props.artistsArray.map((artist, i, array) => {
-                return (
-                        <a className="artist-link" key={artist.id} href={artist.external_urls.spotify}>
-                            <p className="artist-name">
-                                { array.length > 1 && i < array.length - 1 ? artist.name + ',  '  : artist.name}
-                            </p>
-                        </a>
-                        ) 
-                    
-                    }) 
-                )
-    }
-        
+  return (
+    props.artistsArray.map((artist, i, array) => {
+      return (
+        <a className="artist-link" key={artist.id} href={artist.external_urls.spotify}>
+          <p className="artist-name">
+            {array.length > 1 && array.length!==2 && i < array.length - 1  && i !== array.length-2  ? artist.name + ',  ' 
+            :i === array.length-2 ? artist.name + ' &'
+            : artist.name}
+          </p>
+        </a>
+      )
+
+    })
+  )
+}
+
 
 
 export default ArtistName;
