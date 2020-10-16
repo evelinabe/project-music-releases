ad71ece349ee887f08f79abb526b1ef22c1a7881
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2020-10-16 10:02:39 +0200

    comma added between artists

--- code/src/Artist-name.js
+++ code/src/Artist-name.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import data from './data.json';
-
-// const artistName = data.albums.items[0].artists[0];
-
-// const mappedArtistsArray = albumsArray.map((album) => {
-//     const artistsArray = album.artists;
-//     return artistsArray
-// })
 
 
 const ArtistName = (props) => {
         return (
-            props.artistsArray.map((artist, i) => {
-                return ( <a key={artist.id} href={artist.external_urls.spotify}>
-                    <p className="artist-name">
-                        {i > 0 && i < 2 ?  artist.name + ', ' : artist.name}
-                    </p>
-                </a>
-           
-            ) 
+            props.artistsArray.map((artist, i, array) => {
+                return (
+                        <a className="artist-link" key={artist.id} href={artist.external_urls.spotify}>
+                            <p className="artist-name">
+                                { array.length > 1 && i < array.length - 1 ? artist.name + ', '  : artist.name}
+                            </p>
+                        </a>
+                        ) 
                     
-        }) 
-        )
+                    }) 
+                )
     }
         
 
